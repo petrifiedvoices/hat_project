3d4d6b3a753e937ee0d0db4d9d4c70fc6b25f6dc
petrajanouchova <petra.janouchova@gmail.com>
2016-12-21 17:13:41 +0100

    new sql

---
+++ PostgreSQL/GreekFemale_ThracianMale_Intermarriages.sql
@@ -0,0 +1,20 @@
+﻿create temp view inscriptionperson as
+SELECT *
+  FROM inscription_info 
+  JOIN inscriptionperson using (inscriptionkey)
+  JOIN personpersonalname USING(personkey)
+  JOIN personal_name using (namekey);
+
+
+SELECT personkey, inscriptionkey, ethnicity, gender, class
+  FROM inscriptionperson
+ where personkey IN (SELECT personkey                             
+                            FROM inscriptionperson
+                           WHERE ethnicity = 'Greek'
+                             AND gender = 'Female'
+                             and class = 'Personal name')
+   and personkey IN (SELECT personkey                            
+                            FROM inscriptionperson
+                           WHERE ethnicity = 'Thracian'
+                             AND gender = 'Male'
+                             and class = 'Partner name');
