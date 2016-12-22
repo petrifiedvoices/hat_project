c41ff41f2ee7f6b96a16d63c20fd394a787b6590
petrajanouchova <petra.janouchova@gmail.com>
2016-12-22 18:10:38 +0100

    new name analysis of epigraphic person, sql scripts etc.

---
+++ PostgreSQL/Family_GreekPersonal_GreekGrandparent.sql
@@ -0,0 +1,18 @@
+﻿CREATE temp view inscriptionfamily as
+SELECT *
+  FROM inscription_info 
+  JOIN inscriptionperson using (inscriptionkey)
+  JOIN personpersonalname USING(personkey)
+  JOIN personal_name using (namekey);
+
+
+SELECT personkey, inscriptionkey, ethnicity, gender, class
+  FROM inscriptionfamily
+ where personkey IN (SELECT personkey                             
+                            FROM inscriptionfamily
+                           WHERE ethnicity = 'Greek'
+                             and class = 'Personal name')                    
+   and personkey IN (SELECT personkey                            
+                            FROM inscriptionfamily
+                           WHERE ethnicity = 'Greek'
+                             and class = 'Grandparent name');
---
+++ R/Names_analysis.R
@@ -0,0 +1,6 @@
+length(unique(HAT_names_analysis$`Person ID`)) #how many unique persons that meet conditions
+length(unique(HAT_names_analysis$`Heurist ID`)) #how many inscriptions that meet conditions
+
+mean(person)
+median(person)
+table(HAT_names_analysis$Ethnicity)
